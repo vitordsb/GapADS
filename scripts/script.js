b1c324d8e3236e611fa2fd46938090129ef5bd60
vitordsb <vitordsb2019@gmail.com>
2025-04-13 04:03:04 -0300

    new sections and responsive

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+var options = {
+    strings: ["Impulsionamos pequenas empresas ao sucesso digital com soluções personalizadas e resultados mensuráveis.", "Especialistas em marketing digital a mais de 5 anos.", "Agência Digital Gap ADS Sua Porta de Entrada para o Sucesso Online."],
+    typeSpeed: 35,
+    backSpeed: 25,
+    loop: true,
+    showCursor: false
+};
+
+var typed = new Typed("#typed", options);
